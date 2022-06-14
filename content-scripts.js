d0b46d2b77c1a7ba2b4c48557e8603333d6297d3
Rosie <544268513@qq.com>
2022-06-14 23:23:24 +0800

    import button

---
+++ content-scripts.js
@@ -0,0 +1,6 @@
+document.body.style.backgroundColor = 'blue';
+const box = document.querySelector('.product-action');
+let btn = document.createElement('button')
+let btnText=document.createTextNode('import!!')
+btn.appendChild(btnText)
+box.appendChild(btn)
